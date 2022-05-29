62edbdb7e6d1148e12228210c0fbe8750a09aff0
lamph <phamlam090@gmail.com>
2022-05-29 11:43:59 +0700

    add css fw

---
+++ components/header.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function Header(){
+    return (
+        <div className={"header"}>
+            <div className={"logo"}>
+                <Link href={"/"}>
+                    <a>
+                        <span className={"text-cyan-100"}>HOAI LAM</span>
+                    </a>
+                </Link>
+            </div>
+            <
+        </div>
+    )
+}
